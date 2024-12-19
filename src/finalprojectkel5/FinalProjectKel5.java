fab06d21f6ed5bd377615a89f83572245b476539
Lenovo X240 <Lenovo X240@Lenovo-X240>
2024-12-19 12:05:30 +0700

    baru mulai project

---
+++ src/finalprojectkel5/FinalProjectKel5.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalprojectkel5;
+
+/**
+ *
+ * @author Lenovo X240
+ */
+public class FinalProjectKel5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
